681a5376cd4f28adb7c72a107be840de9a2450ec
nata-rozanova80 <nata.rozanova80@gmail.com>
2024-10-30 23:42:03 +0300

    author name corrected

--- dg/news/admin.py
+++ dg/news/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import News_post
 
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'short_description', 'pub_date', 'author')  # Добавьте поле автора в список
+    readonly_fields = ('author',)  # поле автора только для чтения
 
-admin.site.register(News_post)
+    def save_model(self, request, obj, form, change):
+        if not obj.author:  # Установите автора только при создании
+            obj.author = request.user  # Установите текущего пользователя как автора
+        obj.save()
+
+
+admin.site.register(News_post, NewsAdmin)
--- dg/news/models.py
+++ dg/news/models.py
@@ -8,6 +8,10 @@ class News_post(models.Model):
 	text = models.TextField('Новость')
 	pub_date = models.DateTimeField('Дата публикации')
 	#author = models.CharField(User, max_length=50, default='0')  # Связь с моделью User
+	author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)  # Добавлено поле автора, может быть пустым
+	# Новые поля
+	#id_author2 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='author2', blank=True)
+	#author2_name = models.CharField(max_length=100, blank=True)
 
 	def __str__(self):
 		return self.title
